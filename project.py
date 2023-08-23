97470dc50f3ea72e6ccd28a0275baa72ad1c6f63
Mitul Mistry <MitulMistryDev@gmail.com>
2023-08-23 17:04:35 -0500

    Implement basic loop for printing options and getting input

--- project.py
+++ project.py
@@ -4,6 +4,29 @@ import sys
 def main():
     settings = process_cli_args()
 
+    choice = 1
+
+    while (True):
+        if choice == 0:
+            print("Exiting program...")
+            break
+
+        elif choice == 1:
+            options = [
+                "US headlines",
+                "Articles by country",
+                "Articles by source",
+                "Search for articles",
+            ]
+
+            print_options(options)
+
+            choice = get_choice()
+        
+        else:
+            print("Invalid entry. Try again.\n")
+            choice = 1
+
 
 def process_cli_args():
     settings = {}
@@ -19,12 +42,22 @@ def process_cli_args():
     return settings
 
 
-def function_2():
-    ...
+def get_choice(description="Choice: "):
+    str = input(description).strip()
+
+    try:
+        return(int(str))
+    except ValueError:
+        return None
 
 
-def function_n():
-    ...
+def print_options(options):
+    print("Pick an option (number):")
+
+    for i in range(len(options)):
+        print(f"{i + 1}. {options[i]}")
+
+    print("Type 0 to exit")
 
 
 if __name__ == "__main__":
