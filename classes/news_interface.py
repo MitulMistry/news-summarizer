956eea0328655406b48fad85161d539de95e8573
Mitul Mistry <MitulMistryDev@gmail.com>
2023-08-24 16:47:46 -0500

    Restructure NewsInterface instance methods into class methods

--- classes/news_interface.py
+++ classes/news_interface.py
@@ -6,28 +6,26 @@ import urllib.parse
 from datetime import datetime, timedelta
 
 class NewsInterface:
-    def __init__(self):
-        # Load environment variables from .env file for development
-        load_dotenv()
-        self.api_key = os.getenv("NEWS_API_KEY")
+    # Load environment variables from .env file for development
+    load_dotenv()
+    api_key = os.getenv("NEWS_API_KEY")
         
-    
+
+    @classmethod
     def get_articles_by_country(self, country="us", count=5):
         payload = {"country": country, "apiKey": self.api_key}
-
-        response = self.make_request("https://newsapi.org/v2/top-headlines", payload)
-        
+        response = self.make_request("https://newsapi.org/v2/top-headlines", payload)        
         return self.process_response(response, count)
     
 
+    @classmethod
     def get_articles_by_source(self, source="abc-news", count=5):
         payload = {"source": source, "apiKey": self.api_key}
-
-        response = self.make_request("https://newsapi.org/v2/top-headlines", payload)
-        
+        response = self.make_request("https://newsapi.org/v2/top-headlines", payload)        
         return self.process_response(response, count)
 
-    
+
+    @classmethod
     def get_articles_by_search(self, search_str, count=5, days=7):
         date = (datetime.now() - timedelta(days=days)).strftime('%Y-%m-%d')
 
@@ -38,11 +36,11 @@ class NewsInterface:
             "apiKey": self.api_key
         }
 
-        response = self.make_request("https://newsapi.org/v2/everything", payload)
-        
+        response = self.make_request("https://newsapi.org/v2/everything", payload)        
         return self.process_response(response, count)
 
 
+    @classmethod
     def make_request(self, url, params):
         try:
             response = requests.get(url, params=params)
@@ -53,6 +51,7 @@ class NewsInterface:
         return response
 
 
+    @classmethod
     def process_response(self, response, count=5):
         return response.json()["articles"][:count] if response else None
 
