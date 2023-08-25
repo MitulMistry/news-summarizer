2e1b141d0d40ec7b69f2bc28325066ed098aefe8
Mitul Mistry <MitulMistryDev@gmail.com>
2023-08-25 16:22:09 -0500

    Add docstrings to API interface class methods

--- classes/news_interface.py
+++ classes/news_interface.py
@@ -6,6 +6,11 @@ import urllib.parse
 from datetime import datetime, timedelta
 
 class NewsInterface:
+    """
+    The NewsInterface class is used to make API calls to the News API and
+    return articles.
+    """
+
     # Load environment variables from .env file for development
     load_dotenv()
     api_key = os.getenv("NEWS_API_KEY")
@@ -13,6 +18,16 @@ class NewsInterface:
 
     @classmethod
     def get_articles_by_country(self, country="us", count=5):
+        """
+        Makes News API call based on provided country code
+
+        :param country: The country code supported by News API
+        :type country: str
+        :param count: The number of articles meant to be returned (up to 10)
+        :type count: int
+        :returns: A list of articles
+        :rtype: list[dict]
+        """
         payload = {"country": country, "apiKey": self.api_key}
         response = self.make_request("https://newsapi.org/v2/top-headlines", payload)        
         return self.process_response(response, count)
@@ -20,6 +35,16 @@ class NewsInterface:
 
     @classmethod
     def get_articles_by_source(self, source="abc-news", count=5):
+        """
+        Makes News API call based on provided news source code
+
+        :param source: The source code supported by News API
+        :type source: str
+        :param count: The number of articles meant to be returned (up to 10)
+        :type count: int
+        :returns: A list of articles
+        :rtype: list[dict]
+        """
         payload = {"sources": source, "apiKey": self.api_key}
         response = self.make_request("https://newsapi.org/v2/top-headlines", payload)        
         return self.process_response(response, count)
@@ -27,6 +52,18 @@ class NewsInterface:
 
     @classmethod
     def get_articles_by_search(self, search_str, count=5, days=7):
+        """
+        Makes News API call based on a search query
+
+        :param search_str: A search query string
+        :type search_str: str
+        :param count: The number of articles meant to be returned (up to 10)
+        :type count: int
+        :param days: The number of days previous to today to include results for
+        :type days: int
+        :returns: A list of articles
+        :rtype: list[dict]
+        """
         date = (datetime.now() - timedelta(days=days)).strftime('%Y-%m-%d')
 
         payload = {
@@ -42,6 +79,17 @@ class NewsInterface:
 
     @classmethod
     def make_request(self, url, params):
+        """
+        A helper method to initiate an API call using requests
+
+        :param url: The url to make the request to
+        :type url: str
+        :param params: A dict of parameters to be embedded in the request string
+        :type params: dict
+        :raises: RequestException: If request failed
+        :returns: A response object
+        :rtype: requests.Response object
+        """
         try:
             response = requests.get(url, params=params)
             response.raise_for_status()
@@ -53,5 +101,15 @@ class NewsInterface:
 
     @classmethod
     def process_response(self, response, count=5):
+        """
+        A helper method to process and format a Response object from requests
+
+        :param response: The country code supported by News API
+        :type response: requests.Response object
+        :param count: The number of articles meant to be returned (up to 10)
+        :type count: int
+        :returns: A list of articles
+        :rtype: list[dict]
+        """
         return response.json()["articles"][:count] if response else None
 
--- classes/summary_interface.py
+++ classes/summary_interface.py
@@ -3,13 +3,29 @@ import requests
 from dotenv import load_dotenv
 
 class SummaryInterface:
+    """
+    The SummaryInterface class is used to make API calls to the TLDRThis API and
+    return a summary of an article based on a provided URL.
+    """
+    
     # Load environment variables from .env file for development
     load_dotenv()
     api_key = os.getenv("TLDR_API_KEY")
         
 
     @classmethod
-    def get_extractive_summary(self, articleUrl, sentences=3):    
+    def get_extractive_summary(self, articleUrl, sentences=3):
+        """
+        Makes TLDRThis API call based on provided URL. Extracts key sentences from
+        the article.
+
+        :param articleUrl: The URL for the article to be summarized
+        :type articleUrl: str
+        :param sentences: The number of sentences to be returned
+        :type sentences: int
+        :returns: A list of sentences (strings)
+        :rtype: list[str]
+        """
         apiUrl = "https://tldrthis.p.rapidapi.com/v1/model/extractive/summarize-url/"
 
         payload = {
@@ -29,6 +45,19 @@ class SummaryInterface:
 
     @classmethod
     def get_humanlike_summary(self, articleUrl, min_length=50, max_length=100):
+        """
+        Makes TLDRThis API call based on provided URL. Composes an AI summary based
+        on the article.
+
+        :param articleUrl: The URL for the article to be summarized
+        :type articleUrl: str
+        :param min_length: The minimum number of words to be returned
+        :type min_length: int
+        :param min_length: The maximum number of words to be returned
+        :type min_length: int
+        :returns: A list of sentences or paragraphs (strings)
+        :rtype: list[str]
+        """
         apiUrl = "https://tldrthis.p.rapidapi.com/v1/model/abstractive/summarize-url/"
 
         payload = {
@@ -49,6 +78,19 @@ class SummaryInterface:
 
     @classmethod
     def make_request(self, url, payload, headers):
+        """
+        A helper method to initiate an API call using requests
+
+        :param url: The url to make the request to
+        :type url: str
+        :param payload: A dict of parameters to be included in the request
+        :type payload: dict
+        :param payload: A dict of headers to be included in the request
+        :type payload: dict
+        :raises: RequestException: If request failed
+        :returns: A response object
+        :rtype: requests.Response object
+        """
         try:
             response = requests.post(url, json=payload, headers=headers)
             response.raise_for_status()
@@ -61,7 +103,12 @@ class SummaryInterface:
     @classmethod
     def process_response(self, response):
         """
-        Returns a list of sentences/paragraphs, or None if empty response
+        A helper method to process and format a Response object from requests
+
+        :param response: The country code supported by News API
+        :type response: requests.Response object
+        :returns: A list of sentences or paragraphs
+        :rtype: list[str]
         """
         return response.json()["summary"] if response else None
 
